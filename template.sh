e18e844e8121f623fbcab00905e2edd4e4bcfb5a
WaleedAhmedAli <waleedahmedali2002@gmail.com>
2026-02-24 03:37:01 +0500

    folder structure

---
+++ template.sh
@@ -0,0 +1,13 @@
+mkdir -p src
+mkdir -p research
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.py
+touch requirements.py
+
+echo "Directory and files are created sucessfully"
