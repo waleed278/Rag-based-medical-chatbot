29f058b905eeea2dfde2267d9d6afb51516856d7
WaleedAhmedAli <waleedahmedali2002@gmail.com>
2026-02-26 21:43:08 +0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages , setup
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Waleed",
+    author_email="waleedahmedali2002@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
